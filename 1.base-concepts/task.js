f46079dcf0566bd3871cae384f52cf8b410b772b
mac <dmitri-shadri@yandex.ru>
2021-12-11 17:44:01 +0300

    modified task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,33 +1,34 @@
 "use strict";
 function solveEquation(a, b, c) {
   let discriminant = Math.pow(b, 2) - 4 * a * c;
-  let d = discriminant;
   let box = [];
-  for (let i = 0; i < 1; i++) {
-    box.push(i);
-  }
-  if (d < 0) { //2,4,7
+  if (discriminant < 0) {
     box = [];
-  }
-  if (d == 0) { //4,-20,25
+  };
+  if (discriminant == 0) {
     box[0] = -b / (2 * a);
     box.splice(1);
-  }
-  if (d > 0) { //a = 3, b = 8, c = -11
-    box[0] = (-b + Math.sqrt(d)) / (2 * a);
-    box[1] = (-b - Math.sqrt(d)) / (2 * a);
+  };
+  if (discriminant > 0) {
+    box[0] = (-b + Math.sqrt(discriminant)) / (2 * a);
+    box[1] = (-b - Math.sqrt(discriminant)) / (2 * a);
     //box.splice(0);
-  }
+  };
   return box;
-}
+};
 console.log(solveEquation(4, -20, 25));
 console.log(solveEquation(2, 4, 7));
 console.log(solveEquation(3, 8, -11));
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
+"use strict";
+function calculateTotalMortgage(percent, creditTerm, creditAmount, date) {
+  let fractionPercent = percent / 100;
   let totalAmount;
-
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
-}
+  let loanBody = creditAmount - creditTerm;
+  let P = fractionPercent * 1 / 12;
+  let degree = loanBody * (P + (P / ((Math.pow((1 + P), date)) - 1)));
+  let test = Math.pow((1 + P), date);
+  totalAmount = degree * date;
+  return totalAmount.toFixed(2);
+};
+console.log(calculateTotalMortgage(10, 0, 50000, 12));
