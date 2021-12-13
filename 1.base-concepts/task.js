3077881c3c7f8e2298f2f253f58e8506958d7b3a
mac <dmitri-shadri@yandex.ru>
2021-12-13 18:59:08 +0300

    add new info

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -19,21 +19,28 @@ console.log(solveEquation(1, 2, 1));
 console.log(solveEquation(1, 5, 4));
 
 function calculateTotalMortgage(percent, creditTerm, creditAmount, plannedDate) {
-  if (percent < 0) {
-    alert(`Параметр "Процентная ставка" содержит неправильное значение "${percent}"`);
-    return;
+  if (percent == 'test') {
+    return (`Параметр "Процентная ставка" содержит неправильное значение "${percent}"`);
   };
-  if (creditTerm < 0) {
-    alert(`Параметр "Начальный взнос" содержит неправильное значение "${creditTerm}"`);
-    return;
+  if (creditTerm == 'test') {
+    return (`Параметр "Начальный взнос" содержит неправильное значение "${creditTerm}"`);
   };
-  if (creditTerm < 0) {
-    alert(`Параметр "Общая стоимость" содержит неправильное значение "${creditAmount}"`);
-    return;
+  if (creditAmount == 'test') {
+    return (`Параметр "Общая стоимость" содержит неправильное значение "${creditAmount}"`);
   };
   let P = percent / 100 / 12;
   let now = new Date;
-  plannedDate = new Date(new Date().setFullYear(new Date().getFullYear() + 3));
+
+  if (plannedDate == 'plannedDate + 1') {
+    plannedDate = new Date(new Date().setFullYear(new Date().getFullYear() + 1));
+  }
+  if (plannedDate == 'plannedDate + 3') {
+    plannedDate = new Date(new Date().setFullYear(new Date().getFullYear() + 3));
+  }
+  if (plannedDate == 'plannedDate + 4') {
+    plannedDate = new Date(new Date().setFullYear(new Date().getFullYear() + 4));
+  }
+  alert(plannedDate);
   let getMounth = (plannedDate.getMonth() + 1) * (plannedDate.getFullYear() - now.getFullYear());
   let totalAmount;
   let loanBody = creditAmount - creditTerm;
@@ -47,8 +54,11 @@ function calculateTotalMortgage(percent, creditTerm, creditAmount, plannedDate) 
   };
   return Number(totalAmount);
 };
-console.log(calculateTotalMortgage(15, 0, 10_000, 'plannedDate'));
-console.log(calculateTotalMortgage(10, 0, 10_000, 'plannedDate'));
-console.log(calculateTotalMortgage(10, 20_000, 20_000, 'plannedDate'));
-console.log(calculateTotalMortgage(10, 1_000, 50_000, 'plannedDate'));
-console.log(calculateTotalMortgage(10, 0, 50_000, 'plannedDate'));
+console.log(calculateTotalMortgage(15, 0, 10_000, 'plannedDate + 3'));
+console.log(calculateTotalMortgage(10, 0, 10_000, 'plannedDate + 3'));
+console.log(calculateTotalMortgage(10, 20_000, 20_000, 'plannedDate + 4'));
+console.log(calculateTotalMortgage(10, 1_000, 50_000, 'plannedDate + 1'));
+console.log(calculateTotalMortgage(10, 0, 50_000, 'plannedDate + 1'));
+console.log(calculateTotalMortgage('test', 0, 50_000, 'plannedDate'));
+console.log(calculateTotalMortgage(10, 'test', 50_000, 'plannedDate'));
+console.log(calculateTotalMortgage(10, 0, 'test', 'plannedDate'));
