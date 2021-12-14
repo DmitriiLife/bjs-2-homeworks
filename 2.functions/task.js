41d377573cc8223f7c0890c77f58d82eef23a00a
mac <dmitri-shadri@yandex.ru>
2021-12-14 03:46:15 +0300

    first commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,11 +1,31 @@
 // Задание 1
-function getArrayParams(arr) {
-  let min, max, sum, avg;
-
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
+"use strict";
+function getArrayParams(...args) {
+  let data = {};
+  let max = args[0];
+  let min = args[0];
+  let maxMassiv = Math.max.apply(null, args);
+  let minMassiv = Math.min.apply(null, args);
+  let result = args.reduce(function (sum, current) {
+    return sum + current;
+  }, 0);
+  let avgMassiv = result / args.length;
+  for (let i = 0; i < 1; i += 1) {
+    if (max < maxMassiv) {
+      max = maxMassiv;
+    }
+    if (min > minMassiv) {
+      max = minMassiv;
+    }
+  }
+  return data = {
+    min: min,
+    max: max,
+    avg: avgMassiv.toFixed(2),
+  };
 }
+console.log(getArrayParams(-99, 99, 10));
+
 
 // Задание 2
 function worker(arr) {
@@ -21,7 +41,7 @@ function makeWork(arrOfArr, func) {
 
   // Ваш кода
   // for ...
-  
+
   return max;
 }
 
