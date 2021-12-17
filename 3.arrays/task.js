23aa5c39d6fab4ed2c9c3675b9580152dd63cd53
mac <dmitri-shadri@yandex.ru>
2021-12-18 01:28:42 +0300

    first commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,29 @@
-function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
-}
-
+"use strict";
+//Задание 1
+const compareArrays = (arr1, arr2) =>
+  arr1.length === arr2.length &&
+  arr1.every((n, i) => n === arr2[i]);
+  //Задание 2
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  const map1 = arr.map(x => x * 10);
+  let positiveArr = map1.filter(function(i) {
+    return i > 0;
+  });
+  let tripleArr = positiveArr.filter(function(i) {
+    return i % 3 == 0;
+  });
+  return resultArr = tripleArr;
 }
+alert("Задание 1");
+console.log(compareArrays([1, 2, 3], [1, 2, 3]));//true
+console.log(compareArrays([1, 2], [1, 2, 3]));//false
+console.log(compareArrays([1, 2, 3], [3, 2, 1]));//false
+console.log(compareArrays([0, 1, 2], [0, 1]));//false
+console.log(compareArrays([0, 1], [0, 1, 2]));//false
+console.log(compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5]));//false
+alert("Задание 2");
+console.log(advancedFilter([-1, 6, -9, 3]));//[60, 30]
+console.log(advancedFilter([-10, -21, 12, 123]));//[120, 1230]
+console.log(advancedFilter([-1, -2]));//[]
+console.log(advancedFilter([4, 3, 5]));//[30]
