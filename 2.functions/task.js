acc9ea8a11f009eb31225e8b857cc2288b8b8060
mac <dmitri-shadri@yandex.ru>
2021-12-17 22:44:50 +0300

    new tets

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,4 +1,4 @@
-// Задание 1
+// // Задание 1
 "use strict";
 function getArrayParams(...args) {
   let data = {};
@@ -12,31 +12,51 @@ function getArrayParams(...args) {
     avg: Number(avgMassiv.toFixed(2)),
   };
 }
-console.log(getArrayParams(1, 2, 3, -100, 10));
-console.log(getArrayParams(-99, 99, 10));
-
 // Задание 2
 function worker(arr) {
-  let s = 0;
-  arr = arr.flat(Infinity);
-  for (let i = 0; i < arr.length; i++) {
-    s += arr[i];
-  }
+  let s;
+  arr.map(i => s += i, s = 0).reverse()[0]
   return s;
 }
 function makeWork(arrOfArr, func) {
-  let sumMake = worker(arrOfArr);
-  let max = Math.max.apply(null, arrOfArr);
-  return sumMake;
+  let max;
+  if (worker(arrOfArr[1]) >= worker(arrOfArr[0])) {
+    max = worker(arrOfArr[1]);
+  }
+  else {
+    (worker(arrOfArr[1]) <= worker(arrOfArr[0]))
+    max = worker(arrOfArr[0]);
+  };
+  return max;
+
 }
+// Задание 3
+function worker2(arr) {
+  let difference;
+  let difference0 = Math.max.apply(null, arr[0]) - Math.min.apply(null, arr[0]);
+  let difference1 = Math.max.apply(null, arr[1]) - Math.min.apply(null, arr[1]);
+  if (difference1 > difference0) {
+    difference = difference1
+  }
+  else {
+    (difference1 < difference0)
+    difference = difference0
+  };
+  return difference;
+}
+
+alert("Задание 1");
+console.log(getArrayParams(1, 2, 3, -100, 10));
+console.log(getArrayParams(-99, 99, 10));
+alert("Задание 2");
 console.log(makeWork([[1, 2, 3], [4, 5, 6]], worker));//15
 console.log(makeWork([[10, 10, 11], [20, 10]], worker));//31
 console.log(makeWork([[0, 0, 0], [-1, -100]], worker));//0
-
-// Задание 3
-// function worker2(arr) {
-// }
-// Задание 3
-function worker2(arr) {
-  // Ваш код
-}
+alert("Задание 3");
+console.log(makeWork([[10, 20, 30], [-40, -50, -65]], worker2));//25
+console.log(makeWork([[10, 10, 11], [20, 10]], worker2));//10
+console.log(makeWork([[0, 1, 2], [-1, -100]], worker2));//99
+alert("test");
+console.log(worker2([[10, 20, 30], [-40, -50, -65]]));//25
+console.log(worker2([[10, 10, 11], [20, 10]]));//10
+console.log(worker2([[0, 1, 2], [-1, -100]]));//99
