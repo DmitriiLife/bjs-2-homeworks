23bbbfad94a9288a3a606d339553065e87cb2c5e
mac <dmitri-shadri@yandex.ru>
2021-12-29 20:14:03 +0300

    add two task

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,25 +1,59 @@
 "use strict"
-function parseCount(a) {
-  if (a != Number.parseInt(a, 10)) {
-    throw Error('Невалидное значение');
+//Задача 1
+function parseCount(value) {
+  const number = Number.parseInt(value, 10);
+  if (isNaN(number)) {
+    throw TypeError('Невалидное значение');
   };
-  return Number.parseInt(a, 10);
+  return number;
 };
 
-function validateCount(b) {
-  if (b != Number.parseInt(b, 10)) {
-    return new Error('Невалидное значение');
-  } else {
-    return parseCount(b)
-  }
+function validateCount(value) {
+  try { return parseCount(value) }
+  catch (e) { return e };
 };
-console.log(validateCount('ыфва'));
-console.log(parseCount('ыфва'));
-console.log(parseCount('123'));
-console.log(parseCount('012'));
-console.log(validateCount('56'));
-try {
-  console.log(parseCount('ыфва'));
-} finally {
-  console.log(validateCount('ыфва'));
+
+//Задача 2
+class Triangle {
+  constructor(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    if (a + b < c || c + b < a || a + c < b) {
+      throw new Error('Треугольник с такими сторонами не существует');
+    };
+  };
+
+  getPerimeter() {
+    return this.a + this.b + this.c;
+  };
+
+  getArea() {
+    const p = 0.5 * this.getPerimeter();
+    return Number(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+  };
 };
+
+function getTriangle(a, b, c) {
+  try { return new Triangle(a, b, c) }
+  catch (e) {
+    return {
+      getArea() {
+        return 'Ошибка! Треугольник не существует'
+      },
+      getPerimeter() {
+        return 'Ошибка! Треугольник не существует'
+      }
+    };
+  };
+};
+
+// console.log(parseCount('123'));
+// console.log(parseCount('012'));
+// console.log(validateCount('56'));
+// console.log(validateCount('ыфва'));
+// console.log(parseCount('ыфва'));
+let what = new Triangle(1, 2, 1);
+console.log(what);
+console.log(what.getPerimeter());
+console.log(what.getArea());
