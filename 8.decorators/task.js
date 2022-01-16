f5b6d7bc526d70250ee7b3c846f00efbe199b87e
mac <dmitri-shadri@yandex.ru>
2022-01-16 15:21:22 +0300

    changed test

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -2,75 +2,57 @@
 //Задача 1
 let cachingDecoratorNew = (func) => {
   let cache = [];
-  function wrapper(...args) {
+  return function wrapper(...args) {
     const hash = args.toString();
-    //const idx = arr.findIndex(el => el.id === item.id)
-    let idx = cache.findIndex((i) => hash);// ищем элемент, хэш которого равен нашему хэшу
-    alert(idx)
+    let idx = cache.findIndex((item) => item.hash === hash);// ищем элемент, хэш которого равен нашему хэшу
     if (idx in cache) {
-      console.log("из кэша: " + cache[idx]);// индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
-      return "из кэша: " + cache[idx];
-    }
+      console.log("из кэша: " + cache[idx].result);// индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+      return "из кэша: " + cache[idx].result;
+    };
     let result = func(...args);// в кэше результата нет - придётся считать
-    cache.push(hash, result);// добавляем элемент с правильной структурой
-    alert(cache)
+    cache.push({ hash, result });// добавляем элемент с правильной структурой
     if (cache.length > 5) {
       cache.shift();// если слишком много элементов в кэше надо удалить самый старый (первый)
     };
+    cache[hash] = result;
     console.log("вычисляем: " + result);
     return "вычисляем: " + result;
-  }
-  return wrapper;
+  };
 };
-const addThree = (a, b, c) => a + b + c;
-const upgradedAddThree = cachingDecoratorNew(addThree);
-upgradedAddThree(1, 2, 3); // вычисляем: 6
-upgradedAddThree(1, 2, 3); // из кэша: 6 
-upgradedAddThree(2, 2, 3); // вычисляем: 7
-upgradedAddThree(3, 2, 3); // вычисляем: 8
-upgradedAddThree(4, 2, 3); // вычисляем: 9
-upgradedAddThree(5, 2, 3); // вычисляем: 10
-upgradedAddThree(6, 2, 3); // вычисляем: 11   (при этом кэш для 1, 2, 3 уничтожается)
-upgradedAddThree(1, 2, 3); // вычисляем: 6  (снова вычисляем, кэша нет)
+
 //Задача 2
-let debounceDecorator = (func, ms) => {
+function debounceDecoratorNew(func, ms) {
   let timeout;
-  return function (...rest) {
+  let throttled = false;
+  return function wrapper(...args) {
     clearTimeout(timeout);
+    if (!throttled) {
+      func.apply(this, args);
+    };
+    throttled = true;
     timeout = setTimeout(() => {
-      let res = func(...rest)
-      console.log(res)
+      func.apply(this, args);
     }, ms);
   };
 };
 
-const sendSignal = () => console.log("Сигнал отправлен");
-const upgradedSendSignal = debounceDecorator(sendSignal, 2000);
-setTimeout(upgradedSendSignal); // Сигнал отправлен
-setTimeout(upgradedSendSignal, 300); // проигнорировано так как от последнего вызова прошло менее 2000мс (300 - 0 < 2000)
-setTimeout(upgradedSendSignal, 900); // проигнорировано так как времени от последнего вызова прошло: 900-300=600 (600 < 2000)
-setTimeout(upgradedSendSignal, 1200); // проигнорировано так как времени от последнего вызова прошло: 1200-900=300 (300 < 2000)
-setTimeout(upgradedSendSignal, 2300); // проигнорировано так как времени от последнего вызова прошло: 2300-1200=1100 (1100 < 2000)
-setTimeout(upgradedSendSignal, 4400); // Сигнал отправлен так как времени от последнего вызова прошло: 4400-2300=2100 (2100 > 2000)
-setTimeout(upgradedSendSignal, 4500); // Сигнал будет отправлен, так как последний вызов debounce декоратора (спустя 4500 + 2000 = 6500) 6,5с
-
 //Задача 3
-function debounceDecorator2(func) {
+const add = (a, b) => a + b;
+
+function debounceDecorator2(func, ms) {
   let timeout;
-  return function (...rest) {
+  let throttled = false;
+  function wrapper(...args) {
+    wrapper.history.push(args);
     clearTimeout(timeout);
+    if (!throttled) {
+      func.apply(this, args);
+    };
+    throttled = true;
     timeout = setTimeout(() => {
-      let res = func(...rest)
-      console.log(res)
-    });
+      func.apply(this, args);
+    }, ms);
   };
+  wrapper.history = [];
+  return wrapper;
 };
-const sendSignal = () => console.log("Сигнал отправлен");
-const upgradedSendSignal2 = debounceDecorator2(sendSignal, 2000);
-setTimeout(upgradedSendSignal2); // Сигнал отправлен
-setTimeout(upgradedSendSignal2, 300); // проигнорировано так как от последнего вызова прошло менее 2000мс (300 - 0 < 2000)
-setTimeout(upgradedSendSignal2, 900); // проигнорировано так как времени от последнего вызова прошло: 900-300=600 (600 < 2000)
-setTimeout(upgradedSendSignal2, 1200); // проигнорировано так как времени от последнего вызова прошло: 1200-900=300 (300 < 2000)
-setTimeout(upgradedSendSignal2, 2300); // проигнорировано так как времени от последнего вызова прошло: 2300-1200=1100 (1100 < 2000)
-setTimeout(upgradedSendSignal2, 4400); // Сигнал отправлен так как времени от последнего вызова прошло: 4400-2300=2100 (2100 > 2000)
-setTimeout(upgradedSendSignal2, 4500); // Сигнал будет отправлен, так как последний вызов debounce декоратора (спустя 4500 + 2000 = 6500) 6,5с
