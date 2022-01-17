a52afb957ff1a295ab85c00ea7afcd72137104c8
mac <dmitri-shadri@yandex.ru>
2022-01-17 16:07:34 +0300

    changed task 1, 3

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -4,17 +4,16 @@ let cachingDecoratorNew = (func) => {
   let cache = [];
   return function wrapper(...args) {
     const hash = args.toString();
-    let idx = cache.findIndex((item) => item.hash === hash);// ищем элемент, хэш которого равен нашему хэшу
-    if (idx in cache) {
-      console.log("из кэша: " + cache[idx].result);// индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+    let idx = cache.findIndex((item) => item.hash === hash);
+    if (idx !== -1) {
+      console.log("из кэша: " + cache[idx].result);
       return "из кэша: " + cache[idx].result;
     };
-    let result = func(...args);// в кэше результата нет - придётся считать
-    cache.push({ hash, result });// добавляем элемент с правильной структурой
+    let result = func(...args);
+    cache.push({ hash, result });
     if (cache.length > 5) {
-      cache.shift();// если слишком много элементов в кэше надо удалить самый старый (первый)
+      cache.shift();
     };
-    cache[hash] = result;
     console.log("вычисляем: " + result);
     return "вычисляем: " + result;
   };
@@ -38,21 +37,21 @@ function debounceDecoratorNew(func, ms) {
 
 //Задача 3
 const add = (a, b) => a + b;
-
 function debounceDecorator2(func, ms) {
   let timeout;
   let throttled = false;
   function wrapper(...args) {
-    wrapper.history.push(args);
+    wrapper.count++;
     clearTimeout(timeout);
     if (!throttled) {
-      func.apply(this, args);
+      func.call(this, ...args);
     };
     throttled = true;
     timeout = setTimeout(() => {
-      func.apply(this, args);
+      throttled = false;
+      func.call(this, ...args);
     }, ms);
   };
-  wrapper.history = [];
+  wrapper.count = 0;
   return wrapper;
 };
