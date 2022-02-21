e19fa327b800940a949046e638309cb27f7aef4e
mac <dmitri-shadri@yandex.ru>
2022-02-21 13:24:43 +0300

    changed line 40

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 //создал функцию студент
 function Student(name, gender, age) {
   this.name = name;
@@ -8,40 +8,40 @@ function Student(name, gender, age) {
 let student1 = new Student("Andrey", "male", 21);
 let student2 = new Student("Lara", "female", 19);
 //создал предмет
-Student.prototype.setSubject = function(subject) {
+Student.prototype.setSubject = function (subject) {
   this.subject = subject;
-}
+};
 //создал массив оценки
-Student.prototype.addMark = function(mark) {
+Student.prototype.addMark = function (mark) {
   if (this.marks === undefined) {
     this.marks = [mark]; // Первая оценка добавляется в массив
   } else {
     this.marks.push(mark); // Все остальные оценки пушатся
   }
-}
+};
 //создал возможность добавить несколько оценок
-Student.prototype.addMarks = function(mark1, ...rest) {
+Student.prototype.addMarks = function (mark1, ...rest) {
   if (this.marks === undefined) {
     this.marks = [mark1, ...rest]; // Первая оценка добавляется в массив
   } else {
     this.marks.push(mark1, ...rest); // Все остальные оценки пушатся
   }
-}
-//создал вычесление средней арифметической оценки 
-Student.prototype.getAverage = function() {
+};
+//создал вычесление средней арифметической оценки
+Student.prototype.getAverage = function () {
   let sum = 0;
   let total = 0;
   for (let i = 0; i < this.marks.length; i++) {
     sum = sum + this.marks[i];
   }
-  return total = sum / this.marks.length;
-}
+  return (total = sum / this.marks.length);
+};
 //создал удаление
-Student.prototype.exclude = function(reason) {
+Student.prototype.exclude = function (reason) {
   delete this.subject;
   delete this.marks;
-  this.reason = reason;
-}
+  this.excluded = reason;
+};
 student1.setSubject("Algebra");
 student1.addMark(5);
 student1.addMark(4);
@@ -49,6 +49,6 @@ student1.addMark(5);
 student2.setSubject("Geometry");
 student2.addMarks(2, 3, 2);
 console.log(student1.getAverage()); //4.6666
-student2.exclude('low grades');
+student2.exclude("low grades");
 console.log(student1);
 console.log(student2);
